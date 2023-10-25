0c7b56832db8ef5c228ded93e9c9986b1461d2f6
Schuhgorgon <139638657+Schuhgorgon0@users.noreply.github.com>
2023-10-25 18:06:38 +0200

    characterControler korrigiert

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -11,7 +11,7 @@ public class CharacterControler : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical, 0) * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
 
         transform.Translate(movement);
 
