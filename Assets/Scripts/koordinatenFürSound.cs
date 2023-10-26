48ef3388cf0201de36a4dfe9b8949b41beb7e5f6
Schuhgorgon <139638657+Schuhgorgon0@users.noreply.github.com>
2023-10-26 14:49:15 +0200

    koordinatenhöhe für musik fertig

--- Assets/Scripts/koordinatenFürSound.cs
+++ Assets/Scripts/koordinatenFürSound.cs
@@ -1,24 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class restart : MonoBehaviour
+public class koordinatenFürSound : MonoBehaviour
 {
+    public float höhe;
+    float startposition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startposition = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        höhe = transform.position.z - startposition;
     }
-    public void Restart()
-    {
-        SceneManager.LoadScene("MasterScene");
-    }
-    
 }
