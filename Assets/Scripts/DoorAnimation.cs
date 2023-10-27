319aff255ff11de0cd0f29864891320d2361cab9
fridolinderkater <luis.kraus@gmail.com>
2023-10-27 09:17:36 +0200

    keystone asset import

---
+++ Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour
+{
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+    public Animator lAnimator;
+    public Animator rAnimator;
+
+
+    void Start()
+    {
+        lAnimator = leftDoor.GetComponent<Animator>();
+        rAnimator = rightDoor.GetComponent<Animator>();
+
+        openDoorsAnimation();
+    }
+
+    public void openDoorsAnimation()
+    {
+        lAnimator.SetBool("Open01", true);
+        rAnimator.SetBool("Open01", true);
+    }
+
+
+}
